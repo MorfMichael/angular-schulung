09854de5780cc9a0e5dae0d82a5fb9a5a2bf9dfd
Michael Morf <michael.morf@kcc.at>
2022-03-02 14:31:56 +0100

    loading adjustment

--- src/app/person/model/person.service.ts
+++ src/app/person/model/person.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, find, mergeAll, Observable, of } from 'rxjs';
+import { BehaviorSubject, find, mergeAll, Observable, of, tap } from 'rxjs';
 import { Person } from './person';
 
 @Injectable({
@@ -10,7 +10,6 @@ export class PersonService {
 
   constructor(private httpClient: HttpClient) {
     this.url = 'http://localhost:3000/persons';
-    this.initialLoad();
   }
 
   url: string;
@@ -19,7 +18,11 @@ export class PersonService {
   persons$: BehaviorSubject<Person[]> = new BehaviorSubject(this.persons);
 
   getPersons(): Observable<Person[]> {
-    return this.persons$;
+    if (this.persons.length == 0) {
+      return this.initialLoad();
+    } else {
+      return this.persons$;
+    }
   }
 
   getPerson(id: number): Observable<Person | undefined> {
@@ -27,32 +30,27 @@ export class PersonService {
   }
 
   savePerson(person: Person): Observable<Person | undefined> {
-
-    console.log(person.id);
     let filtered = this.persons.filter(el => el.id != person.id);
     let isNew = !this.persons.some(x => x.id == person.id);
     this.persons = [...filtered, person];
     this.persons$.next(this.persons);
 
-    console.log(isNew);
-
     if (isNew) {
-      console.log('post');
       this.httpClient.post(this.url, person).subscribe();
     } else {
-      console.log('put');
       this.httpClient.put(this.url + '/' + person.id, person).subscribe();
     }
 
     return of(person);
   }
 
-  initialLoad() {
-    this.httpClient.get<Person[]>(this.url).subscribe(
-      data => {
-        this.persons = data;
-        this.persons$.next(this.persons);
-      }
+  initialLoad(): Observable<Person[]> {
+    return this.httpClient.get<Person[]>(this.url).pipe(
+      tap(
+        data => {
+          this.persons = data;
+          this.persons$.next(this.persons);
+        })
     )
   }
 }
